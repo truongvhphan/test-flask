0d04973deb3da5ee5556a1da65c4a9d993620fa0
truonghvphan <truongphv@gmail.com>
2025-07-28 12:19:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'  
+def home():
+    return render_template("home.html")
+
+@app.route('/shopping', methods=['GET'])
+def shopping():
+    search_query = request.args.get('search')
+    return render_template('shopping.html', search=search_query)
+
+@app.route('/service')
+def service():
+    return render_template("service.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/chatbot', strict_slashes=False)
+def chatbot():
+    return render_template("chatbot.html")
+
